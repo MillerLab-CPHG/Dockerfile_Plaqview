ca2d927d338bd71a8ba4ddabe8d9d74b28642fde
Wei Feng Ma <weimasemail@gmail.com>
2022-10-03 10:33:08 -0400

    stable update r 4.1.2

---
+++ Launch_Rocker_Singularity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# specify path to container
+SIF=/project/cphg-millerlab/WeiMa/Project/PlaqView/Singularity/rstudio_latest.sif
+
+# specify path to tmp directory created in previous section
+TMPDIR=/project/cphg-millerlab/WeiMa/Project/PlaqView/Singularity # your choice
+
+module load singularity
+singularity exec \
+    -B $TMPDIR/var/lib:/var/lib/rstudio-server \
+    -B $TMPDIR/var/run:/var/run/rstudio-server \
+    -B $TMPDIR/tmp:/tmp \
+    $SIF \
+    rserver --www-address=127.0.0.1 --server-user=$USER
+
+
+
+# run the below ONCE
+# TMPDIR=/project/cphg-millerlab/WeiMa/Project/PlaqView/Singularity # your choice
+# mkdir -p $TMPDIR/tmp/rstudio-server
+# uuidgen > $TMPDIR/tmp/rstudio-server/secure-cookie-key
+# chmod 600 $TMPDIR/tmp/rstudio-server/secure-cookie-key
+# mkdir -p $TMPDIR/var/{lib,run}
